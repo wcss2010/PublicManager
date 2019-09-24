108f18250b1f8676863ec5f5195431c19c919085
wcss <wcss2020@gmail.com>
2019-09-24 14:24:00 +0800

    ...

---
+++ Code/PublicManager/PublicManager/Modules/BaseModuleController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+using DevExpress.XtraBars.Ribbon;
+
+namespace PublicManager.Modules
+{
+    /// <summary>
+    /// 模块控制器
+    /// </summary>
+    public abstract partial class BaseModuleController : UserControl
+    {
+        public BaseModuleController()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 获得用于显示在顶部工具栏的RibbonPage
+        /// </summary>
+        /// <returns></returns>
+        public abstract RibbonPage[] GetTopBarPages();
+
+        /// <summary>
+        /// 设置/获得用于显示内容的控件
+        /// </summary>
+        public virtual ScrollableControl DisplayControl { get; set; }
+    }
+}
