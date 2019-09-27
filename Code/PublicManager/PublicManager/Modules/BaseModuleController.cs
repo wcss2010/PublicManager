8b472227f9e9549768c508ccb5fbef4f323f16e9
wcss <wcss2020@gmail.com>
2019-09-27 16:09:31 +0800

    ...

--- Code/PublicManager/PublicManager/Modules/BaseModuleController.cs
+++ Code/PublicManager/PublicManager/Modules/BaseModuleController.cs
@@ -48,5 +48,109 @@ namespace PublicManager.Modules
         /// 停止
         /// </summary>
         public virtual void stop() { }
+
+        /// <summary>
+        /// 输出数据表格
+        /// </summary>
+        /// <param name="workbook">工作文档</param>
+        /// <param name="normalStyle">普通样式(用于表格内容)</param>
+        /// <param name="boldStyle">粗体样式(用于表格头部)</param>
+        /// <param name="table">表格数据</param>
+        public static void writeSheet(NPOI.XSSF.UserModel.XSSFWorkbook workbook, NPOI.SS.UserModel.ICellStyle normalStyle, NPOI.SS.UserModel.ICellStyle boldStyle, DataTable table)
+        {
+            //创建Sheet页
+            NPOI.SS.UserModel.ISheet sheet = workbook.CreateSheet();
+
+            //行号
+            int rowIndex = 0;
+
+            //是否需要输出表头
+            bool isNeedCreateHeader = true;
+
+            //输出数据到Excel
+            foreach (DataRow rowData in table.Rows)
+            {
+                //忽略空数据行
+                if (rowData.ItemArray == null || rowData.ItemArray.Length != table.Columns.Count)
+                {
+                    continue;
+                }
+
+                //列号
+                int colIndex = 0;
+
+                //Excel行
+                NPOI.SS.UserModel.IRow row = null;
+
+                //是否需要输入表头
+                if (isNeedCreateHeader)
+                {
+                    isNeedCreateHeader = false;
+
+                    //创建行
+                    row = sheet.CreateRow(rowIndex);
+                    //输出列名到Excel
+                    colIndex = 0;
+                    foreach (DataColumn kvp in table.Columns)
+                    {
+                        //列名
+                        //创建列
+                        NPOI.SS.UserModel.ICell cell = row.CreateCell(colIndex);
+                        //设置样式
+                        cell.CellStyle = boldStyle;
+                        //设置数据
+                        cell.SetCellValue(kvp.ColumnName);
+                        colIndex++;
+                    }
+                    rowIndex++;
+                }
+
+                //创建行
+                row = sheet.CreateRow(rowIndex);
+                //输出列值到Excel
+                colIndex = 0;
+                foreach (object val in rowData.ItemArray)
+                {
+                    //列值
+                    //创建列
+                    NPOI.SS.UserModel.ICell cell = row.CreateCell(colIndex);
+                    //设置样式
+                    cell.CellStyle = normalStyle;
+                    //设置数据
+                    //判断是否为空
+                    if (val != null)
+                    {
+                        //不为空
+                        //判断是否为RTF内容
+                        if (val.GetType().Name.Equals(typeof(NPOI.XSSF.UserModel.XSSFRichTextString).Name))
+                        {
+                            //RTF内容
+                            cell.SetCellValue((NPOI.XSSF.UserModel.XSSFRichTextString)val);
+                        }
+                        else
+                        {
+                            //文本内容
+                            cell.SetCellValue(val.ToString());
+                        }
+                    }
+                    else
+                    {
+                        //为空
+                        cell.SetCellValue(string.Empty);
+                    }
+                    colIndex++;
+                }
+                rowIndex++;
+            }
+
+            //Excel列宽自动适应
+            if (table.Rows.Count >= 1 && sheet.GetRow(0) != null)
+            {
+                for (int k = 0; k < sheet.GetRow(0).Cells.Count; k++)
+                {
+                    sheet.AutoSizeColumn(k);
+                }
+            }
+        }
     }
 }
