56feb484a6995890cd34ff429630bfb987e25682
wcss <wcss2020@gmail.com>
2019-11-18 09:24:54 +0800

    ...

---
+++ Code/PublicManager/PublicManager/Modules/Teacher/TeacherManager/TeacherManageModuleController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PublicManager.Modules.Teacher.TeacherManager
+{
+    public partial class TeacherManageModuleController : UserControl
+    {
+        public TeacherManageModuleController()
+        {
+            InitializeComponent();
+        }
+    }
+}
