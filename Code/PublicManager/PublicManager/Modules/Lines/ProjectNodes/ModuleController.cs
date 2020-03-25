ffabb41ccc7618fefd092036fdb1cc2679a0256d
wcss <wcss2020@gmail.com>
2020-03-25 17:57:44 +0800

    ...

--- Code/PublicManager/PublicManager/Modules/Lines/ProjectNodes/ModuleController.cs
+++ Code/PublicManager/PublicManager/Modules/Lines/ProjectNodes/ModuleController.cs
@@ -129,33 +129,117 @@ namespace PublicManager.Modules.Lines.ProjectNodes
 
         private void btnImportExcelA_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
         {
-            OpenFileDialog sfd = new OpenFileDialog();
-            sfd.FileName = string.Empty;
-            sfd.Filter = "*.xlsx|*.xlsx";
-            if (sfd.ShowDialog() == DialogResult.OK)
+            int[] selecteds = gvDetail.GetSelectedRows();
+            if (selecteds != null && selecteds.Length >= 1)
             {
-                try
+                object objValue = gvDetail.GetRowCellValue(selecteds[0], "row6");
+                if (objValue != null && !string.IsNullOrEmpty(objValue.ToString()))
                 {
-                    DataSet ds = ExcelHelper.ExcelToDataSet(sfd.FileName);
-                    if (ds != null && ds.Tables.Count >= 2)
+                    string nodeId = objValue.ToString();
+
+                    MoneySends mss = ConnectionManager.Context.table("MoneySends").where("MSID='" + nodeId + "'").select("*").getItem<MoneySends>(new MoneySends());
+                    if (mss != null && string.IsNullOrEmpty(mss.MSID))
                     {
-                        foreach (DataTable dt in ds.Tables)
+                        return;
+                    }
+
+                    OpenFileDialog sfd = new OpenFileDialog();
+                    sfd.FileName = string.Empty;
+                    sfd.Filter = "*.xlsx|*.xlsx";
+                    if (sfd.ShowDialog() == DialogResult.OK)
+                    {
+                        try
                         {
-                            switch (dt.TableName)
+                            DataSet ds = ExcelHelper.ExcelToDataSet(sfd.FileName);
+                            if (ds != null && ds.Tables.Count >= 2)
                             {
-                                case "a":
+                                //清除数据
+                                ConnectionManager.Context.table("Contact_Table4").where("NodeID='" + nodeId + "'").delete();
+                                ConnectionManager.Context.table("Contact_Table5").where("NodeID='" + nodeId + "'").delete();
 
-                                    break;
-                                case "b":
+                                foreach (DataTable dt in ds.Tables)
+                                {
+                                    switch (dt.TableName)
+                                    {
+                                        case "项目经费使用情况":
+                                            #region 导入 项目经费使用情况
+                                            foreach (DataColumn dc in dt.Columns)
+                                            {
+                                                if (dc.ColumnName == "科目名称")
+                                                {
+                                                    continue;
+                                                }
 
-                                    break;
+                                                //添数据
+                                                int rowIndexx = 0;
+                                                foreach (DataRow dr in dt.Rows)
+                                                {
+                                                    rowIndexx++;
+
+                                                    //取值
+                                                    string valStr = dr[dc.ColumnName] != null ? dr[dc.ColumnName].ToString() : string.Empty;
+                                                    Contact_Table4 ct = new Contact_Table4();
+                                                    ct.TID = Guid.NewGuid().ToString();
+                                                    ct.CatalogID = mss.CatalogID;
+                                                    ct.ProjectID = mss.ProjectID;
+                                                    ct.NodeID = mss.MSID;
+                                                    ct.ModuleName = dc.ColumnName + "xxxxx" + rowIndexx;
+                                                    ct.ModuleValue = valStr;
+                                                    ct.copyTo(ConnectionManager.Context.table("Contact_Table4")).insert();
+                                                }
+                                            }
+                                            #endregion
+                                            break;
+                                        case "课题经费拨付与支出情况":
+                                            #region 导入 课题经费拨付与支出情况
+                                            foreach (DataRow dr in dt.Rows)
+                                            {
+                                                string value1 = dr["课题名称"] != null ? dr["课题名称"].ToString() : string.Empty;
+                                                string value2 = dr["课题负责单位"] != null ? dr["课题负责单位"].ToString() : string.Empty;
+                                                string value3 = dr["课题合同总价款"] != null ? dr["课题合同总价款"].ToString() : string.Empty;
+                                                string value4 = dr["课题应拨经费"] != null ? dr["课题应拨经费"].ToString() : string.Empty;
+                                                string value5 = dr["课题经费拨付时间"] != null ? dr["课题经费拨付时间"].ToString() : string.Empty;
+                                                string value6 = dr["课题已拨经费"] != null ? dr["课题已拨经费"].ToString() : string.Empty;
+                                                string value7 = dr["课题支出经费"] != null ? dr["课题支出经费"].ToString() : string.Empty;
+                                                string value8 = dr["课题应拨未拨经费"] != null ? dr["课题应拨未拨经费"].ToString() : string.Empty;
+
+                                                if (string.IsNullOrEmpty(value1))
+                                                {
+                                                    continue;
+                                                }
+
+                                                Subject subjectObj = ConnectionManager.Context.table("Subject").where("SubjectName='" + value1 + "'").select("*").getItem<Subject>(new Subject());
+                                                if (subjectObj != null && string.IsNullOrEmpty(subjectObj.SubjectID))
+                                                {
+                                                    continue;
+                                                }
+
+                                                Contact_Table5 ct = new Contact_Table5();
+                                                ct.TID = Guid.NewGuid().ToString();
+                                                ct.CatalogID = mss.CatalogID;
+                                                ct.ProjectID = mss.ProjectID;
+                                                ct.NodeID = mss.MSID;
+                                                ct.SubjectID = subjectObj.SubjectID;
+                                                ct.SubjectWorkUnit = value2;
+                                                ct.SubjectTotalMoney = decimal.Parse(value3);
+                                                ct.SubjectSendMoney = decimal.Parse(value4);
+                                                ct.SubjectSendTime = DateTime.Parse(value5);
+                                                ct.SubjectSendedMoney = decimal.Parse(value6);
+                                                ct.SubjectUseMoney = decimal.Parse(value7);
+                                                ct.SubjectNoSendMoney = decimal.Parse(value8);
+                                                ct.copyTo(ConnectionManager.Context.table("Contact_Table5")).insert();
+                                            }
+                                            #endregion
+                                            break;
+                                    }
+                                }
                             }
                         }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("对不起，Excel导入失败！Ex:" + ex.ToString());
+                        }
                     }
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show("对不起，Excel导入失败！Ex:" + ex.ToString());
                 }
             }
         }
