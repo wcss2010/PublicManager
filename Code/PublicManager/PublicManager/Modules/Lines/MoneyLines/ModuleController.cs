6cee2ffcdb30546f768f93c7d343fa12084cd442
wcss <wcss2020@gmail.com>
2020-03-25 20:06:14 +0800

    ...

--- Code/PublicManager/PublicManager/Modules/Lines/MoneyLines/ModuleController.cs
+++ Code/PublicManager/PublicManager/Modules/Lines/MoneyLines/ModuleController.cs
@@ -69,35 +69,100 @@ namespace PublicManager.Modules.Lines.MoneyLines
 
         private void tvProjectList_AfterSelect(object sender, TreeViewEventArgs e)
         {
-            mlDetail.setCellValue(2, 4, "fdsafda");
-            //dgvDetail.Rows.Clear();
-            //List<List<object>> objectList = new List<List<object>>();
+            clearMoneyTable();
 
-            //if (e.Node.Tag is Catalog)
-            //{
-            //    //项目年度列表
-            //    Catalog catalogObj = (Catalog)e.Node.Tag;
+            List<List<object>> objectList = new List<List<object>>();
+            if (e.Node.Tag is Catalog)
+            {
+                Catalog catalogObj = (Catalog)e.Node.Tag;
 
-            //    List<MoneySends> subList = ConnectionManager.Context.table("MoneySends").where("(CatalogID = '" + catalogObj.CatalogID + "' and ProjectID = '" + catalogObj.CatalogID + "')").select("*").getList<MoneySends>(new MoneySends());
-            //    int indexx = 0;
-            //    foreach (MoneySends mss in subList)
-            //    {
-            //        indexx++;
-            //        List<object> cells = new List<object>();
-            //        cells.Add(indexx.ToString());
-            //        cells.Add(mss.SendRule);
-            //        cells.Add(mss.WillTime.ToString("yyyy年MM月dd日"));
-            //        cells.Add(mss.TotalMoney);
-            //        cells.Add(mss.MemoText);
+                //显示总的经费
+            }
+            else if (e.Node.Tag is MoneySends)
+            {
+                MoneySends moneySendObj = (MoneySends)e.Node.Tag;
 
-            //        objectList.Add(cells);
-            //    }
-            //}
+                //显示节点经费
+                List<Contact_Table4> moneyss = ConnectionManager.Context.table("Contact_Table4").where("NodeID='" + moneySendObj.MSID + "'").select("*").getList<Contact_Table4>(new Contact_Table4());
+                foreach (Contact_Table4 mObj in moneyss)
+                {
+                    try
+                    {
+                        if (mObj.ModuleName != null)
+                        {
+                            if (mObj.ModuleName.StartsWith("项目合同"))
+                            {
+                                string[] tttt = mObj.ModuleName.Split(new string[] { "xxxxx" }, StringSplitOptions.None);
+                                if (tttt != null && tttt.Length >= 2)
+                                {
+                                    mlDetail.setCellValue(2, 4+ (int.Parse(tttt[1]) - 1), mObj.ModuleValue);
+                                }
+                            }
+                            else if (mObj.ModuleName.StartsWith("本阶段支出经费"))
+                            {
+                                string[] tttt = mObj.ModuleName.Split(new string[] { "xxxxx" }, StringSplitOptions.None);
+                                if (tttt != null && tttt.Length >= 2)
+                                {
+                                    mlDetail.setCellValue(3, 4 + (int.Parse(tttt[1]) - 1), mObj.ModuleValue);
+                                }
+                            }
+                            else if (mObj.ModuleName.StartsWith("备注"))
+                            {
+                                string[] tttt = mObj.ModuleName.Split(new string[] { "xxxxx" }, StringSplitOptions.None);
+                                if (tttt != null && tttt.Length >= 2)
+                                {
+                                    mlDetail.setCellValue(9, 4 + (int.Parse(tttt[1]) - 1), mObj.ModuleValue);
+                                }
+                            }
+                        }
+                    }
+                    catch (Exception ex) { }
+                }
+            }
+        }
 
-            //foreach (List<object> lxItem in objectList)
-            //{
-            //    dgvDetail.Rows.Add(lxItem.ToArray());
-            //}
+        private void clearMoneyTable()
+        {
+            #region 设置标题栏
+            //合同价款
+            mlDetail.setCellValue(1, 1, "0");
+
+            //累计预算经费
+            mlDetail.setCellValue(3, 1, "0");
+
+            //累计到位经费
+            mlDetail.setCellValue(5, 1, "0");
+
+            //累计支出经费
+            mlDetail.setCellValue(7, 1, "0");
+
+            //空,暂时没用到
+            mlDetail.setCellValue(8, 1, string.Empty);
+            //空,暂时没用到
+            mlDetail.setCellValue(9, 1, string.Empty);
+            #endregion
+
+            #region 清理经费表
+            int defaultX = 2;
+            int defaultY = 4;
+            for (int sy = 0; sy < 15; sy++)
+            {
+                for (int sx = 0; sx < 8; sx++)
+                {
+                    int nowX = defaultX + sx;
+                    int nowY = defaultY + sy;
+
+                    if (sx == 7)
+                    {
+                        mlDetail.setCellValue(nowX, nowY, string.Empty);
+                    }
+                    else
+                    {
+                        mlDetail.setCellValue(nowX, nowY, string.Empty);
+                    }
+                }
+            }
+            #endregion
         }
     }
 }
