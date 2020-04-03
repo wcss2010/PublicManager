284edd648c835da86cb04abf24b281a1f16699fd
wcss <wcss2020@gmail.com>
2020-04-03 14:09:38 +0800

    ...

--- Code/PublicManager/PublicManager/DB/Entitys/Teacher.cs
+++ Code/PublicManager/PublicManager/DB/Entitys/Teacher.cs
@@ -21,8 +21,12 @@ namespace PublicManager.DB.Entitys
             query.set("TSex", TSex);
             query.set("TPhone", TPhone);
             query.set("TJob", TJob);
+            query.set("TJobTopic", TJobTopic);
             query.set("TUnit", TUnit);
             query.set("TRange", TRange);
+            query.set("TDirection", TDirection);
+            query.set("TSource", TSource);
+            query.set("TInnerJob", TInnerJob);
 
             return query;
         }
@@ -33,19 +37,27 @@ namespace PublicManager.DB.Entitys
         public string TSex { get; set; }
         public string TPhone { get; set; }
         public string TJob { get; set; }
+        public string TJobTopic { get; set; }
         public string TUnit { get; set; }
         public string TRange { get; set; }
+        public string TDirection { get; set; }
+        public string TSource { get; set; }
+        public string TInnerJob { get; set; }
 
         public override void bind(Noear.Weed.GetHandlerEx source)
         {
-            TeacherID = source("TeacherID").value<string>(Guid.NewGuid().ToString());
+            TeacherID = source("TeacherID").value<string>("");
             TName = source("TName").value<string>("");
             TIDCard = source("TIDCard").value<string>("");
             TSex = source("TSex").value<string>("");
             TPhone = source("TPhone").value<string>("");
             TJob = source("TJob").value<string>("");
+            TJobTopic = source("TJobTopic").value<string>("");
             TUnit = source("TUnit").value<string>("");
             TRange = source("TRange").value<string>("");
+            TDirection = source("TDirection").value<string>("");
+            TSource = source("TSource").value<string>("");
+            TInnerJob = source("TInnerJob").value<string>("");
         }
 
         public override Noear.Weed.IBinder clone()
