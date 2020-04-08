5d3a0bcf0235317ec73d0bcd2a178c1fa2f7432e
wcss <wcss2020@gmail.com>
2020-04-08 19:50:31 +0800

    ...

---
+++ Code/PublicManager/PublicManager/Modules/SearchRulePanel.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace PublicManager.Modules
+{
+    partial class SearchRulePanel
+    {
+        /// <summary> 
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 组件设计器生成的代码
+
+        /// <summary> 
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.plRules = new DevExpress.XtraEditors.GroupControl();
+            this.tplControls = new System.Windows.Forms.TableLayoutPanel();
+            this.txtKey1 = new DevExpress.XtraEditors.TextEdit();
+            this.panelControl1 = new DevExpress.XtraEditors.PanelControl();
+            this.cbDisplayReporter = new System.Windows.Forms.CheckBox();
+            this.cbDisplayContract = new System.Windows.Forms.CheckBox();
+            this.lblKey2 = new System.Windows.Forms.Label();
+            this.lblKey1 = new System.Windows.Forms.Label();
+            this.panelControl2 = new DevExpress.XtraEditors.PanelControl();
+            this.btnReset = new DevExpress.XtraEditors.SimpleButton();
+            this.btnExportTo = new DevExpress.XtraEditors.SimpleButton();
+            this.btnSearch = new DevExpress.XtraEditors.SimpleButton();
+            this.txtKey2 = new System.Windows.Forms.ComboBox();
+            this.fplCheckList = new System.Windows.Forms.FlowLayoutPanel();
+            ((System.ComponentModel.ISupportInitialize)(this.plRules)).BeginInit();
+            this.plRules.SuspendLayout();
+            this.tplControls.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.txtKey1.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).BeginInit();
+            this.panelControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl2)).BeginInit();
+            this.panelControl2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // plRules
+            // 
+            this.plRules.Controls.Add(this.tplControls);
+            this.plRules.Controls.Add(this.fplCheckList);
+            this.plRules.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.plRules.Location = new System.Drawing.Point(0, 0);
+            this.plRules.Name = "plRules";
+            this.plRules.Size = new System.Drawing.Size(979, 85);
+            this.plRules.TabIndex = 0;
+            this.plRules.Text = "搜索条件";
+            // 
+            // tplControls
+            // 
+            this.tplControls.ColumnCount = 3;
+            this.tplControls.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 24.7085F));
+            this.tplControls.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 75.2915F));
+            this.tplControls.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tplControls.Controls.Add(this.txtKey1, 1, 0);
+            this.tplControls.Controls.Add(this.panelControl1, 2, 0);
+            this.tplControls.Controls.Add(this.lblKey2, 0, 1);
+            this.tplControls.Controls.Add(this.lblKey1, 0, 0);
+            this.tplControls.Controls.Add(this.panelControl2, 2, 1);
+            this.tplControls.Controls.Add(this.txtKey2, 1, 1);
+            this.tplControls.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tplControls.Location = new System.Drawing.Point(152, 21);
+            this.tplControls.Name = "tplControls";
+            this.tplControls.RowCount = 2;
+            this.tplControls.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tplControls.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tplControls.Size = new System.Drawing.Size(825, 62);
+            this.tplControls.TabIndex = 1;
+            // 
+            // txtKey1
+            // 
+            this.txtKey1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtKey1.Location = new System.Drawing.Point(155, 3);
+            this.txtKey1.Name = "txtKey1";
+            this.txtKey1.Properties.NullValuePrompt = "请输入XXXXXX！";
+            this.txtKey1.Properties.NullValuePromptShowForEmptyValue = true;
+            this.txtKey1.Properties.ShowNullValuePromptWhenFocused = true;
+            this.txtKey1.Size = new System.Drawing.Size(459, 20);
+            this.txtKey1.TabIndex = 3;
+            this.txtKey1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtKey1_KeyDown);
+            // 
+            // panelControl1
+            // 
+            this.panelControl1.Controls.Add(this.cbDisplayReporter);
+            this.panelControl1.Controls.Add(this.cbDisplayContract);
+            this.panelControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelControl1.Location = new System.Drawing.Point(620, 3);
+            this.panelControl1.Name = "panelControl1";
+            this.panelControl1.Size = new System.Drawing.Size(202, 25);
+            this.panelControl1.TabIndex = 2;
+            // 
+            // cbDisplayReporter
+            // 
+            this.cbDisplayReporter.AutoSize = true;
+            this.cbDisplayReporter.Checked = true;
+            this.cbDisplayReporter.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.cbDisplayReporter.Dock = System.Windows.Forms.DockStyle.Right;
+            this.cbDisplayReporter.Location = new System.Drawing.Point(138, 2);
+            this.cbDisplayReporter.Name = "cbDisplayReporter";
+            this.cbDisplayReporter.Size = new System.Drawing.Size(62, 21);
+            this.cbDisplayReporter.TabIndex = 6;
+            this.cbDisplayReporter.Text = "未立项";
+            this.cbDisplayReporter.UseVisualStyleBackColor = true;
+            this.cbDisplayReporter.CheckedChanged += new System.EventHandler(this.cbDisplayContract_CheckedChanged);
+            // 
+            // cbDisplayContract
+            // 
+            this.cbDisplayContract.AutoSize = true;
+            this.cbDisplayContract.Checked = true;
+            this.cbDisplayContract.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.cbDisplayContract.Dock = System.Windows.Forms.DockStyle.Left;
+            this.cbDisplayContract.Location = new System.Drawing.Point(2, 2);
+            this.cbDisplayContract.Name = "cbDisplayContract";
+            this.cbDisplayContract.Padding = new System.Windows.Forms.Padding(12, 0, 0, 0);
+            this.cbDisplayContract.Size = new System.Drawing.Size(74, 21);
+            this.cbDisplayContract.TabIndex = 5;
+            this.cbDisplayContract.Text = "已立项";
+            this.cbDisplayContract.UseVisualStyleBackColor = true;
+            this.cbDisplayContract.CheckedChanged += new System.EventHandler(this.cbDisplayContract_CheckedChanged);
+            // 
+            // lblKey2
+            // 
+            this.lblKey2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblKey2.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblKey2.Location = new System.Drawing.Point(3, 31);
+            this.lblKey2.Name = "lblKey2";
+            this.lblKey2.Size = new System.Drawing.Size(146, 31);
+            this.lblKey2.TabIndex = 1;
+            this.lblKey2.Text = "搜索关键字：";
+            this.lblKey2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.lblKey2.Visible = false;
+            // 
+            // lblKey1
+            // 
+            this.lblKey1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblKey1.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblKey1.Location = new System.Drawing.Point(3, 0);
+            this.lblKey1.Name = "lblKey1";
+            this.lblKey1.Size = new System.Drawing.Size(146, 31);
+            this.lblKey1.TabIndex = 1;
+            this.lblKey1.Text = "搜索关键字：";
+            this.lblKey1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // panelControl2
+            // 
+            this.panelControl2.Controls.Add(this.btnReset);
+            this.panelControl2.Controls.Add(this.btnExportTo);
+            this.panelControl2.Controls.Add(this.btnSearch);
+            this.panelControl2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelControl2.Location = new System.Drawing.Point(620, 34);
+            this.panelControl2.Name = "panelControl2";
+            this.panelControl2.Size = new System.Drawing.Size(202, 25);
+            this.panelControl2.TabIndex = 4;
+            // 
+            // btnReset
+            // 
+            this.btnReset.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnReset.Location = new System.Drawing.Point(67, 2);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(59, 21);
+            this.btnReset.TabIndex = 1;
+            this.btnReset.Text = "重置";
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnExportTo
+            // 
+            this.btnExportTo.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnExportTo.Location = new System.Drawing.Point(126, 2);
+            this.btnExportTo.Name = "btnExportTo";
+            this.btnExportTo.Size = new System.Drawing.Size(74, 21);
+            this.btnExportTo.TabIndex = 0;
+            this.btnExportTo.Text = "导出Excel";
+            this.btnExportTo.Click += new System.EventHandler(this.btnExportTo_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnSearch.Location = new System.Drawing.Point(2, 2);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(65, 21);
+            this.btnSearch.TabIndex = 0;
+            this.btnSearch.Text = "搜索";
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // txtKey2
+            // 
+            this.txtKey2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtKey2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.txtKey2.Location = new System.Drawing.Point(155, 34);
+            this.txtKey2.Name = "txtKey2";
+            this.txtKey2.Size = new System.Drawing.Size(459, 22);
+            this.txtKey2.TabIndex = 5;
+            this.txtKey2.Visible = false;
+            // 
+            // fplCheckList
+            // 
+            this.fplCheckList.AutoScroll = true;
+            this.fplCheckList.Dock = System.Windows.Forms.DockStyle.Left;
+            this.fplCheckList.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.fplCheckList.Location = new System.Drawing.Point(2, 21);
+            this.fplCheckList.Name = "fplCheckList";
+            this.fplCheckList.Size = new System.Drawing.Size(150, 62);
+            this.fplCheckList.TabIndex = 0;
+            // 
+            // SearchRulePanel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.plRules);
+            this.Name = "SearchRulePanel";
+            this.Size = new System.Drawing.Size(979, 85);
+            ((System.ComponentModel.ISupportInitialize)(this.plRules)).EndInit();
+            this.plRules.ResumeLayout(false);
+            this.tplControls.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.txtKey1.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).EndInit();
+            this.panelControl1.ResumeLayout(false);
+            this.panelControl1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl2)).EndInit();
+            this.panelControl2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.GroupControl plRules;
+        private System.Windows.Forms.FlowLayoutPanel fplCheckList;
+        private System.Windows.Forms.TableLayoutPanel tplControls;
+        private System.Windows.Forms.Label lblKey1;
+        private DevExpress.XtraEditors.PanelControl panelControl1;
+        private DevExpress.XtraEditors.TextEdit txtKey1;
+        private DevExpress.XtraEditors.PanelControl panelControl2;
+        private DevExpress.XtraEditors.SimpleButton btnExportTo;
+        private DevExpress.XtraEditors.SimpleButton btnSearch;
+        private System.Windows.Forms.CheckBox cbDisplayReporter;
+        private System.Windows.Forms.CheckBox cbDisplayContract;
+        private DevExpress.XtraEditors.SimpleButton btnReset;
+        private System.Windows.Forms.Label lblKey2;
+        private System.Windows.Forms.ComboBox txtKey2;
+    }
+}
---
+++ Code/PublicManager/PublicManager/Modules/SearchRulePanel.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace PublicManager.Modules
+{
+    public delegate void SearchClickDelegate(object sender,EventArgs args);
+    public delegate void ResetClickDelegate(object sender, EventArgs args);
+    public delegate void ExportToClickDelegate(object sender, EventArgs args);
+
+    public partial class SearchRulePanel : XtraUserControl
+    {
+        private string strCatalogIDFilterString = " and CatalogID in (select CatalogID from Catalog)";
+        /// <summary>
+        /// 目录表过滤条件
+        /// </summary>
+        public string CatalogIDFilterString
+        {
+            get { return strCatalogIDFilterString; }
+        }
+
+        /// <summary>
+        /// 搜索条件1标签
+        /// </summary>
+        public Label Key1Label { get { return lblKey1; } }
+
+        /// <summary>
+        /// 搜索条件1内容
+        /// </summary>
+        public TextEdit Key1EditControl { get { return txtKey1; } }
+
+        /// <summary>
+        /// 搜索条件2标签
+        /// </summary>
+        public Label Key2Label { get { return lblKey2; } }
+
+        /// <summary>
+        /// 搜索条件2内容
+        /// </summary>
+        public System.Windows.Forms.ComboBox Key2EditControl { get { return txtKey2; } }
+
+        public event SearchClickDelegate OnSearchClick;
+        public event ResetClickDelegate OnResetClick;
+        public event ExportToClickDelegate OnExportToClick;
+
+        private string key1FieldString = string.Empty;
+        /// <summary>
+        /// 搜索条件1的可搜索字段，用";"分割
+        /// </summary>
+        public string Key1FieldString
+        {
+            get { return key1FieldString; }
+            set
+            {
+                key1FieldString = value;
+
+                if (string.IsNullOrEmpty(value))
+                {
+                    return;
+                }
+                else
+                {
+                    fplCheckList.Controls.Clear();
+                    string[] teams = value.Split(new string[] { ";" }, StringSplitOptions.None);
+                    foreach (string ss in teams)
+                    {
+                        CheckEdit ce = new CheckEdit();
+                        ce.Name = ss;
+                        ce.Checked = true;
+                        fplCheckList.Controls.Add(ce);
+                    }
+
+                    Key1EditControl.Properties.NullValuePrompt = "请输入" + value.Replace(";", "或") + "！";
+                }
+            }
+        }
+
+        public SearchRulePanel()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 获得当前选择的条件
+        /// </summary>
+        /// <returns></returns>
+        public Dictionary<string, bool> getRuleCheckedDict()
+        {
+            Dictionary<string, bool> results = new Dictionary<string, bool>();
+            foreach (Control c in fplCheckList.Controls)
+            {
+                if (c is CheckEdit)
+                {
+                    CheckEdit ce = (CheckEdit)c;
+                    if (string.IsNullOrEmpty(ce.Name))
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        results[ce.Name.Trim()] = ce.Checked;
+                    }
+                }
+            }
+            return results;
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            if (OnSearchClick != null)
+            {
+                OnSearchClick(this, new EventArgs());
+            }
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            foreach (Control c in fplCheckList.Controls)
+            {
+                if (c is CheckEdit)
+                {
+                    CheckEdit ce = (CheckEdit)c;
+                    ce.Checked = true;
+                }
+            }
+            Key1EditControl.Text = string.Empty;
+            if (Key2EditControl.Items.Count >= 1)
+            {
+                Key2EditControl.SelectedIndex = 0;
+            }
+
+            if (OnResetClick != null)
+            {
+                OnResetClick(this, new EventArgs());
+            }
+        }
+
+        private void btnExportTo_Click(object sender, EventArgs e)
+        {
+            if (OnExportToClick != null)
+            {
+                OnExportToClick(this, new EventArgs());
+            }
+        }
+
+        private void cbDisplayContract_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cbDisplayContract.Checked && cbDisplayReporter.Checked)
+            {
+                strCatalogIDFilterString = " and CatalogID in (select CatalogID from Catalog)";
+            }
+            else if (cbDisplayContract.Checked)
+            {
+                strCatalogIDFilterString = " and CatalogID in (select CatalogID from Catalog where CatalogType = '合同书')";
+            }
+            else if (cbDisplayReporter.Checked)
+            {
+                strCatalogIDFilterString = " and CatalogID in (select CatalogID from Catalog where CatalogType = '建议书')";
+            }
+            else
+            {
+                strCatalogIDFilterString = " and CatalogID in (select CatalogID from Catalog)";
+            }
+        }
+
+        private void txtKey1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                btnSearch.PerformClick();
+            }
+        }
+    }
+}
