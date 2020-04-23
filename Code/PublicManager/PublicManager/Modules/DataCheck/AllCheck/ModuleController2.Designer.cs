ff3c0e5c67ecdeabca888734082a588bf1eceeec
wcss <wcss2020@gmail.com>
2020-04-23 20:01:09 +0800

    ...

--- Code/PublicManager/PublicManager/Modules/DataCheck/AllCheck/ModuleController2.Designer.cs
+++ Code/PublicManager/PublicManager/Modules/DataCheck/AllCheck/ModuleController2.Designer.cs
@@ -34,14 +34,16 @@
             this.gridColumn2 = new DevExpress.XtraGrid.Columns.GridColumn();
             this.gridColumn3 = new DevExpress.XtraGrid.Columns.GridColumn();
             this.gridColumn4 = new DevExpress.XtraGrid.Columns.GridColumn();
-            this.gridColumn12 = new DevExpress.XtraGrid.Columns.GridColumn();
             this.gridColumn5 = new DevExpress.XtraGrid.Columns.GridColumn();
-            this.gridColumn10 = new DevExpress.XtraGrid.Columns.GridColumn();
-            this.gridColumn11 = new DevExpress.XtraGrid.Columns.GridColumn();
             this.gridColumn6 = new DevExpress.XtraGrid.Columns.GridColumn();
             this.gridColumn7 = new DevExpress.XtraGrid.Columns.GridColumn();
             this.gridColumn8 = new DevExpress.XtraGrid.Columns.GridColumn();
             this.gridColumn9 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn10 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn11 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn12 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn13 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn14 = new DevExpress.XtraGrid.Columns.GridColumn();
             ((System.ComponentModel.ISupportInitialize)(this.gcGrid)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dgvDetail)).BeginInit();
             this.SuspendLayout();
@@ -64,14 +66,16 @@
             this.gridColumn2,
             this.gridColumn3,
             this.gridColumn4,
-            this.gridColumn12,
             this.gridColumn5,
-            this.gridColumn10,
-            this.gridColumn11,
             this.gridColumn6,
             this.gridColumn7,
             this.gridColumn8,
-            this.gridColumn9});
+            this.gridColumn9,
+            this.gridColumn10,
+            this.gridColumn11,
+            this.gridColumn12,
+            this.gridColumn13,
+            this.gridColumn14});
             this.dgvDetail.GridControl = this.gcGrid;
             this.dgvDetail.Name = "dgvDetail";
             this.dgvDetail.OptionsFind.AlwaysVisible = true;
@@ -80,15 +84,19 @@
             // 
             // gridColumn1
             // 
-            this.gridColumn1.Caption = "版本";
+            this.gridColumn1.Caption = "项目领域";
             this.gridColumn1.FieldName = "row1";
             this.gridColumn1.Name = "gridColumn1";
+            this.gridColumn1.Visible = true;
+            this.gridColumn1.VisibleIndex = 0;
             // 
             // gridColumn2
             // 
-            this.gridColumn2.Caption = "类型";
+            this.gridColumn2.Caption = "计划批次";
             this.gridColumn2.FieldName = "row2";
             this.gridColumn2.Name = "gridColumn2";
+            this.gridColumn2.Visible = true;
+            this.gridColumn2.VisibleIndex = 1;
             // 
             // gridColumn3
             // 
@@ -96,7 +104,7 @@
             this.gridColumn3.FieldName = "row3";
             this.gridColumn3.Name = "gridColumn3";
             this.gridColumn3.Visible = true;
-            this.gridColumn3.VisibleIndex = 0;
+            this.gridColumn3.VisibleIndex = 2;
             // 
             // gridColumn4
             // 
@@ -104,71 +112,87 @@
             this.gridColumn4.FieldName = "row4";
             this.gridColumn4.Name = "gridColumn4";
             this.gridColumn4.Visible = true;
-            this.gridColumn4.VisibleIndex = 1;
-            // 
-            // gridColumn12
-            // 
-            this.gridColumn12.Caption = "保密等级";
-            this.gridColumn12.FieldName = "row12";
-            this.gridColumn12.Name = "gridColumn12";
-            this.gridColumn12.Visible = true;
-            this.gridColumn12.VisibleIndex = 2;
+            this.gridColumn4.VisibleIndex = 3;
             // 
             // gridColumn5
             // 
-            this.gridColumn5.Caption = "课题负责人";
+            this.gridColumn5.Caption = "项目负责人";
             this.gridColumn5.FieldName = "row5";
             this.gridColumn5.Name = "gridColumn5";
             this.gridColumn5.Visible = true;
-            this.gridColumn5.VisibleIndex = 3;
-            // 
-            // gridColumn10
-            // 
-            this.gridColumn10.Caption = "课题负责人座机";
-            this.gridColumn10.FieldName = "row10";
-            this.gridColumn10.Name = "gridColumn10";
-            this.gridColumn10.Visible = true;
-            this.gridColumn10.VisibleIndex = 4;
-            // 
-            // gridColumn11
-            // 
-            this.gridColumn11.Caption = "课题负责人手机";
-            this.gridColumn11.FieldName = "row11";
-            this.gridColumn11.Name = "gridColumn11";
-            this.gridColumn11.Visible = true;
-            this.gridColumn11.VisibleIndex = 5;
+            this.gridColumn5.VisibleIndex = 4;
             // 
             // gridColumn6
             // 
-            this.gridColumn6.Caption = "课题负责单位";
+            this.gridColumn6.Caption = "课题负责人";
             this.gridColumn6.FieldName = "row6";
             this.gridColumn6.Name = "gridColumn6";
             this.gridColumn6.Visible = true;
-            this.gridColumn6.VisibleIndex = 6;
+            this.gridColumn6.VisibleIndex = 5;
             // 
             // gridColumn7
             // 
-            this.gridColumn7.Caption = "所属大单位";
+            this.gridColumn7.Caption = "项目牵头单位";
             this.gridColumn7.FieldName = "row7";
             this.gridColumn7.Name = "gridColumn7";
             this.gridColumn7.Visible = true;
-            this.gridColumn7.VisibleIndex = 7;
+            this.gridColumn7.VisibleIndex = 6;
             // 
             // gridColumn8
             // 
-            this.gridColumn8.Caption = "单位地址";
+            this.gridColumn8.Caption = "课题负责单位";
             this.gridColumn8.FieldName = "row8";
             this.gridColumn8.Name = "gridColumn8";
             this.gridColumn8.Visible = true;
-            this.gridColumn8.VisibleIndex = 8;
+            this.gridColumn8.VisibleIndex = 7;
             // 
             // gridColumn9
             // 
-            this.gridColumn9.Caption = "课题总经费";
+            this.gridColumn9.Caption = "项目牵头单位地址";
             this.gridColumn9.FieldName = "row9";
             this.gridColumn9.Name = "gridColumn9";
             this.gridColumn9.Visible = true;
-            this.gridColumn9.VisibleIndex = 9;
+            this.gridColumn9.VisibleIndex = 8;
+            // 
+            // gridColumn10
+            // 
+            this.gridColumn10.Caption = "课题负责单位所在地址";
+            this.gridColumn10.FieldName = "row10";
+            this.gridColumn10.Name = "gridColumn10";
+            this.gridColumn10.Visible = true;
+            this.gridColumn10.VisibleIndex = 9;
+            // 
+            // gridColumn11
+            // 
+            this.gridColumn11.Caption = "牵头单位所属单位";
+            this.gridColumn11.FieldName = "row11";
+            this.gridColumn11.Name = "gridColumn11";
+            this.gridColumn11.Visible = true;
+            this.gridColumn11.VisibleIndex = 10;
+            // 
+            // gridColumn12
+            // 
+            this.gridColumn12.Caption = "项目负责单位所属单位";
+            this.gridColumn12.FieldName = "row12";
+            this.gridColumn12.Name = "gridColumn12";
+            this.gridColumn12.Visible = true;
+            this.gridColumn12.VisibleIndex = 11;
+            // 
+            // gridColumn13
+            // 
+            this.gridColumn13.Caption = "合同审查等级";
+            this.gridColumn13.FieldName = "row13";
+            this.gridColumn13.Name = "gridColumn13";
+            this.gridColumn13.Visible = true;
+            this.gridColumn13.VisibleIndex = 12;
+            // 
+            // gridColumn14
+            // 
+            this.gridColumn14.Caption = "节点评估时间";
+            this.gridColumn14.FieldName = "row14";
+            this.gridColumn14.Name = "gridColumn14";
+            this.gridColumn14.Visible = true;
+            this.gridColumn14.VisibleIndex = 13;
             // 
             // ModuleController2
             // 
@@ -191,13 +215,15 @@
         private DevExpress.XtraGrid.Columns.GridColumn gridColumn2;
         private DevExpress.XtraGrid.Columns.GridColumn gridColumn3;
         private DevExpress.XtraGrid.Columns.GridColumn gridColumn4;
-        private DevExpress.XtraGrid.Columns.GridColumn gridColumn12;
         private DevExpress.XtraGrid.Columns.GridColumn gridColumn5;
-        private DevExpress.XtraGrid.Columns.GridColumn gridColumn10;
-        private DevExpress.XtraGrid.Columns.GridColumn gridColumn11;
         private DevExpress.XtraGrid.Columns.GridColumn gridColumn6;
         private DevExpress.XtraGrid.Columns.GridColumn gridColumn7;
         private DevExpress.XtraGrid.Columns.GridColumn gridColumn8;
         private DevExpress.XtraGrid.Columns.GridColumn gridColumn9;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn10;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn11;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn12;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn13;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn14;
     }
 }
--- Code/PublicManager/PublicManager/Modules/DataCheck/AllCheck/ModuleController2.cs
+++ Code/PublicManager/PublicManager/Modules/DataCheck/AllCheck/ModuleController2.cs
@@ -6,6 +6,8 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using PublicManager.DB.Entitys;
+using PublicManager.DB;
 
 namespace PublicManager.Modules.DataCheck.AllCheck
 {
@@ -19,7 +21,7 @@ namespace PublicManager.Modules.DataCheck.AllCheck
 
             dgvDetail.OptionsBehavior.Editable = false;
             dgvDetail.OptionsView.AllowCellMerge = true;
-            cma = new DEGridViewCellMergeAdapter(dgvDetail, new string[] { "row3" });
+            cma = new DEGridViewCellMergeAdapter(dgvDetail, new string[] { "row1", "row2", "row3" });
         }
 
         public override void start()
@@ -35,7 +37,81 @@ namespace PublicManager.Modules.DataCheck.AllCheck
 
         private void loadData()
         {
-            
+            DataTable dt = getTempDataTable("row", 14);
+
+            List<Project> projList = ConnectionManager.Context.table("Project").select("*").getList<Project>(new Project());
+            foreach (Project proj in projList)
+            {
+                //项目类型
+                string catalogType = ConnectionManager.Context.table("Catalog").where("CatalogID='" + proj.CatalogID + "'").select("CatalogType").getValue<string>("未知");
+
+                List<Subject> subList = ConnectionManager.Context.table("Subject").where("CatalogID = '" + proj.CatalogID + "' and ProjectID = '" + proj.ProjectID + "'").select("*").getList<Subject>(new Subject());
+                foreach (Subject sub in subList)
+                {
+                    List<object> cells = new List<object>();
+
+                    //项目领域
+                    cells.Add(proj.Domains);
+
+                    //计划批次
+                    cells.Add(proj.TaskNumber);
+
+                    //项目名称
+                    cells.Add(proj.ProjectName);
+
+                    //课题名称
+                    cells.Add(sub.SubjectName);
+
+                    //项目负责人
+                    Person masterPerson = ConnectionManager.Context.table("Person").where("CatalogID = '" + proj.CatalogID + "' and SubjectID = '' and IsProjectMaster = 'true'").select("*").getItem<Person>(new Person());
+                    if (masterPerson != null && masterPerson.PersonID != null && masterPerson.PersonID.Length >= 1)
+                    {
+                        cells.Add(masterPerson.PersonName);
+                    }
+                    else
+                    {
+                        cells.Add(string.Empty);
+                    }
+
+                    //课题负责人
+                    Person subjectPerson = ConnectionManager.Context.table("Person").where("CatalogID = '" + proj.CatalogID + "' and SubjectID = '" + sub.SubjectID + "' and JobInProject = '负责人'").select("*").getItem<Person>(new Person());
+                    if (string.IsNullOrEmpty(subjectPerson.PersonID))
+                    {
+                        cells.Add(string.Empty);
+                    }
+                    else
+                    {
+                        cells.Add(subjectPerson.PersonName);
+                    }
+
+                    //项目牵头单位
+                    cells.Add(proj.DutyUnit);
+
+                    //课题负责单位
+                    cells.Add(sub.DutyUnit);
+
+                    //项目牵头单位地址
+                    cells.Add(proj.DutyUnitAddress);
+
+                    //课题负责单位所在地址
+                    cells.Add(sub.DutyUnitAddress);
+
+                    //项目牵头单位所属单位
+                    cells.Add(proj.DutyUnitOrg);
+
+                    //课题负责单位所属单位
+                    cells.Add(sub.DutyUnitOrg);
+
+                    //合同审查等级
+                    cells.Add(proj.ContactCheckLevelA);
+
+                    //节点评估时间
+                    cells.Add("");
+
+                    dt.Rows.Add(cells.ToArray());
+                }
+            }
+            gcGrid.DataSource = dt;
         }
     }
 }
