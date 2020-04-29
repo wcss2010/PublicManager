0f6695930838f4287ee638cccbe2605bb4ff016d
wcss <wcss2020@gmail.com>
2020-04-29 11:16:31 +0800

    ...

--- Code/PublicManager/PublicManager/Modules/DataCheck/AddressCheck/ModuleController.cs
+++ Code/PublicManager/PublicManager/Modules/DataCheck/AddressCheck/ModuleController.cs
@@ -91,8 +91,58 @@ namespace PublicManager.Modules.DataCheck.AddressCheck
             List<Project> projList = MakeSQLWithSearchRule.getProjectList(srpSearch);
             foreach (Project proj in projList)
             {
+                int subCounttt = 0;
+                
+                #region 生成从表数据
+                if (proj != null && !string.IsNullOrEmpty(proj.ProjectID))
+                {
+                    List<Subject> subList = ConnectionManager.Context.table("Subject").where("CatalogID = '" + proj.CatalogID + "' and ProjectID = '" + proj.ProjectID + "'").select("*").getList<Subject>(new Subject());
+                    foreach (Subject sub in subList)
+                    {
+                        if (string.IsNullOrEmpty(srpSearch.Key1EditControl.Text) || ((srpSearch.getUsingRuleCount() == 0 || srpSearch.isUsingRule("课题")) && MakeSQLWithSearchRule.isDisplayData(typeof(Subject).Name, sub.SubjectID)) || srpSearch.isUsingRule("项目") == true)
+                        {
+                            if (srpSearch.Key2EditControl.Text == "全部" || sub.DutyUnitOrg == srpSearch.Key2EditControl.Text)
+                            {
+                                subCounttt++;
+
+                                cells = new List<object>();
+                                cells.Add(sub.SubjectName);
+                                Person personObj = ConnectionManager.Context.table("Person").where("CatalogID = '" + proj.CatalogID + "' and SubjectID = '" + sub.SubjectID + "' and JobInProject = '负责人'").select("*").getItem<Person>(new Person());
+                                if (string.IsNullOrEmpty(personObj.PersonID))
+                                {
+                                    cells.Add(string.Empty);
+                                }
+                                else
+                                {
+                                    cells.Add(personObj.PersonName);
+                                }
+                                cells.Add(sub.DutyUnit);
+                                cells.Add(sub.DutyUnitOrg);
+                                cells.Add(sub.DutyUnitAddress);
+                                cells.Add(proj.ProjectID);
+
+                                if (string.IsNullOrEmpty(personObj.PersonID))
+                                {
+                                    cells.Add(string.Empty);
+                                    cells.Add(string.Empty);
+                                }
+                                else
+                                {
+                                    cells.Add(personObj.Telephone);
+                                    cells.Add(personObj.Mobilephone);
+                                }
+
+                                cells.Add(sub.SecretLevel);
+
+                                detailDt.Rows.Add(cells.ToArray());
+                            }
+                        }
+                    }
+                }
+                #endregion
+
                 #region 主表数据
-                if (srpSearch.Key2EditControl.Text == "全部" || proj.DutyUnitOrg == srpSearch.Key2EditControl.Text)
+                if (srpSearch.Key2EditControl.Text == "全部" || proj.DutyUnitOrg == srpSearch.Key2EditControl.Text || subCounttt >= 1)
                 {
                     cells = new List<object>();
                     cells.Add(ConnectionManager.Context.table("Catalog").where("CatalogID='" + proj.CatalogID + "'").select("CatalogVersion").getValue<string>("未知"));
@@ -137,52 +187,6 @@ namespace PublicManager.Modules.DataCheck.AddressCheck
                     cells.Add(proj.ProjectRange);
 
                     masterDt.Rows.Add(cells.ToArray());
-                }
-                #endregion
-
-                #region 生成从表数据
-                if (proj != null && !string.IsNullOrEmpty(proj.ProjectID))
-                {
-                    List<Subject> subList = ConnectionManager.Context.table("Subject").where("CatalogID = '" + proj.CatalogID + "' and ProjectID = '" + proj.ProjectID + "'").select("*").getList<Subject>(new Subject());
-                    foreach (Subject sub in subList)
-                    {
-                        if (string.IsNullOrEmpty(srpSearch.Key1EditControl.Text) || ((srpSearch.getUsingRuleCount() == 0 || srpSearch.isUsingRule("课题")) && MakeSQLWithSearchRule.isDisplayData(typeof(Subject).Name, sub.SubjectID)) || srpSearch.isUsingRule("项目") == true)
-                        {
-                            if (srpSearch.Key2EditControl.Text == "全部" || sub.DutyUnitOrg == srpSearch.Key2EditControl.Text)
-                            {
-                                cells = new List<object>();
-                                cells.Add(sub.SubjectName);
-                                Person personObj = ConnectionManager.Context.table("Person").where("CatalogID = '" + proj.CatalogID + "' and SubjectID = '" + sub.SubjectID + "' and JobInProject = '负责人'").select("*").getItem<Person>(new Person());
-                                if (string.IsNullOrEmpty(personObj.PersonID))
-                                {
-                                    cells.Add(string.Empty);
-                                }
-                                else
-                                {
-                                    cells.Add(personObj.PersonName);
-                                }
-                                cells.Add(sub.DutyUnit);
-                                cells.Add(sub.DutyUnitOrg);
-                                cells.Add(sub.DutyUnitAddress);
-                                cells.Add(proj.ProjectID);
-
-                                if (string.IsNullOrEmpty(personObj.PersonID))
-                                {
-                                    cells.Add(string.Empty);
-                                    cells.Add(string.Empty);
-                                }
-                                else
-                                {
-                                    cells.Add(personObj.Telephone);
-                                    cells.Add(personObj.Mobilephone);
-                                }
-
-                                cells.Add(sub.SecretLevel);
-
-                                detailDt.Rows.Add(cells.ToArray());
-                            }
-                        }
-                    }
                 }
                 #endregion
             }
