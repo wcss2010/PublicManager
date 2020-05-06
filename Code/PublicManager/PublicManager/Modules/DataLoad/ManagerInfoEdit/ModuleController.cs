1698d0cbdf2867291683ecf15b2add0bd4492bae
wcss <wcss2020@gmail.com>
2020-05-06 14:13:19 +0800

    ...

---
+++ Code/PublicManager/PublicManager/Modules/DataLoad/ManagerInfoEdit/ModuleController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using PublicManager.DB.Entitys;
+using PublicManager.DB;
+using PublicManager.Modules.Lines.ProjectLines.Froms;
+
+namespace PublicManager.Modules.DataLoad.ManagerInfoEdit
+{
+    public partial class ModuleController : BaseModuleController
+    {
+        private DEGridViewCellMergeAdapter cma;
+
+        public ModuleController()
+        {
+            InitializeComponent();
+
+            dgvDetail.OptionsBehavior.Editable = false;
+            dgvDetail.OptionsView.AllowCellMerge = true;
+            cma = new DEGridViewCellMergeAdapter(dgvDetail, new string[] { "row3", "row4", "row5", "row6", "row23" });
+
+            srpSearch.IsDisplayReporterData = false;
+        }
+
+        public override void start()
+        {
+            base.start();
+
+            this.DisplayControl.Controls.Clear();
+            this.Dock = DockStyle.Fill;
+            this.DisplayControl.Controls.Add(this);
+
+            this.loadData();
+        }
+
+        private void loadData()
+        {
+
+        }
+
+        private void srpSearch_OnSearchClick(object sender, EventArgs args)
+        {
+            DataTable dt = getTempDataTable("row", 24);
+
+            List<Project> projList = MakeSQLWithSearchRule.getProjectList(srpSearch);
+            foreach (Project proj in projList)
+            {
+                List<object> cells = new List<object>();
+                cells.Add(ConnectionManager.Context.table("Catalog").where("CatalogID='" + proj.CatalogID + "'").select("CatalogVersion").getValue<string>("未知"));
+                cells.Add(ConnectionManager.Context.table("Catalog").where("CatalogID='" + proj.CatalogID + "'").select("CatalogType").getValue<string>("未知"));
+                cells.Add(proj.Domains);
+                cells.Add(proj.TaskNumber);
+                cells.Add(proj.ProjectNumber);
+                cells.Add(proj.ProjectName);
+                cells.Add(proj.TotalMoney);
+                cells.Add(proj.TotalTime);
+                cells.Add(proj.SecretLevel);
+                //显示仅为项目负责人
+                Person masterPerson = ConnectionManager.Context.table("Person").where("CatalogID = '" + proj.CatalogID + "' and SubjectID = '' and IsProjectMaster = 'true'").select("*").getItem<Person>(new Person());
+                if (masterPerson != null && masterPerson.PersonID != null && masterPerson.PersonID.Length >= 1)
+                {
+                    cells.Add(masterPerson.PersonName);
+                }
+                else
+                {
+                    cells.Add(string.Empty);
+                }
+                cells.Add(proj.DutyUnit);
+                cells.Add(proj.DutyUnitOrg);
+                cells.Add(proj.DutyUnitAddress);
+
+                cells.Add(proj.OKQuestionMemo);
+                cells.Add(proj.OKCheckA);
+                cells.Add(proj.OKCheckB);
+                cells.Add(proj.ContactCheckLevelA);
+                cells.Add(proj.ContactCheckLevelB);
+                cells.Add(proj.Memo);
+
+                cells.Add(proj.ProjectID);
+
+                if (masterPerson != null && masterPerson.PersonID != null && masterPerson.PersonID.Length >= 1)
+                {
+                    cells.Add(masterPerson.Telephone);
+                    cells.Add(masterPerson.Mobilephone);
+                }
+                else
+                {
+                    cells.Add(string.Empty);
+                    cells.Add(string.Empty);
+                }
+
+                cells.Add(proj.ProjectRange);
+
+                cells.Add(proj.DutyNormalUnit);
+
+                dt.Rows.Add(cells.ToArray());
+            }
+            gcGrid.DataSource = dt;
+        }
+
+        private void srpSearch_OnResetClick(object sender, EventArgs args)
+        {
+            srpSearch.search();
+        }
+
+        private void srpSearch_OnExportToClick(object sender, EventArgs args)
+        {
+            exportToExcelWithDevExpress(dgvDetail);
+        }
+
+        private void srpSearch_OnCustomButtonClick(object sender, CustomButtonEventArgs args)
+        {
+            if (args.ButtonName == "录入审核结果")
+            {
+                int[] selecteds = dgvDetail.GetSelectedRows();
+                if (selecteds != null && selecteds.Length >= 1)
+                {
+                    object objValue = dgvDetail.GetRowCellValue(selecteds[0], "row20");
+                    if (objValue != null && !string.IsNullOrEmpty(objValue.ToString()))
+                    {
+                        string projectId = objValue.ToString();
+
+                        Project proj = ConnectionManager.Context.table("Project").where("ProjectID='" + projectId + "'").select("*").getItem<Project>(new Project());
+                        if (new CheckEditForm(proj).ShowDialog() == DialogResult.OK)
+                        {
+                            srpSearch.search();
+                        }
+                    }
+                }
+            }
+            else if (args.ButtonName == "列选择器")
+            {
+                dgvDetail.ShowCustomization();
+            }
+        }
+
+        private void dgvDetail_RowCellStyle(object sender, DevExpress.XtraGrid.Views.Grid.RowCellStyleEventArgs e)
+        {
+            int[] rowIndexxx = dgvDetail.GetSelectedRows();
+            if (rowIndexxx != null && rowIndexxx.Length == 1)
+            {
+                //第一行  
+                if (e.RowHandle == rowIndexxx[0])
+                {
+                    if (cma.MergeFieldNameList.Contains(e.Column.FieldName))
+                    {
+                        return;
+                    }
+
+                    e.Appearance.BackColor = Color.LightSkyBlue;
+                }
+                else
+                {
+                    e.Appearance.Reset();
+                }
+            }
+            else
+            {
+                e.Appearance.Reset();
+            }
+        }
+    }
+}
---
+++ Code/PublicManager/PublicManager/Modules/DataLoad/NormalUnitDict/ModuleController.Designer.cs
@@ -0,0 +1,396 @@
+﻿namespace PublicManager.Modules.DataLoad.NormalUnitDict
+{
+    partial class ModuleController
+    {
+        /// <summary> 
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 组件设计器生成的代码
+
+        /// <summary> 
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.srpSearch = new PublicManager.Modules.SearchRulePanel();
+            this.gcGrid = new DevExpress.XtraGrid.GridControl();
+            this.dgvDetail = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.gridColumn1 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn2 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn23 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn3 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn4 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn5 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn6 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn7 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn8 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn9 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn10 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn22 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn21 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn11 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn24 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn12 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn13 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn14 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn15 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn16 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn17 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn18 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn19 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn20 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gcGrid)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDetail)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.AutoSize = true;
+            this.panel1.Controls.Add(this.srpSearch);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(3, 18);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1414, 54);
+            this.panel1.TabIndex = 2;
+            // 
+            // srpSearch
+            // 
+            this.srpSearch.CustomButtonsNames = "结果内容搜索;录入审核结果;列选择器";
+            this.srpSearch.DisplayGridControl = this.gcGrid;
+            this.srpSearch.Dock = System.Windows.Forms.DockStyle.Top;
+            this.srpSearch.IsDisplayCatalogTypePanel = false;
+            this.srpSearch.IsDisplayCheckListPanel = false;
+            this.srpSearch.IsDisplayContractData = true;
+            this.srpSearch.IsDisplayCustomButtonPanel = true;
+            this.srpSearch.IsDisplayKey1Panel = true;
+            this.srpSearch.IsDisplayKey2Panel = false;
+            this.srpSearch.IsDisplayKey3OR4Panel = false;
+            this.srpSearch.IsDisplayKey3Panel = false;
+            this.srpSearch.IsDisplayKey4Panel = false;
+            this.srpSearch.IsDisplayReporterData = true;
+            this.srpSearch.Key1FieldString = "项目名称";
+            this.srpSearch.Key1PanelWidth = 960;
+            this.srpSearch.Key2PanelWidth = 296;
+            this.srpSearch.Key3OR4PanelWidth = 946;
+            this.srpSearch.Key3PanelWidth = 492;
+            this.srpSearch.Key4PanelWidth = 597;
+            this.srpSearch.Location = new System.Drawing.Point(0, 0);
+            this.srpSearch.Name = "srpSearch";
+            this.srpSearch.RightButtonPanelWidth = 450;
+            this.srpSearch.Size = new System.Drawing.Size(1414, 54);
+            this.srpSearch.TabIndex = 10;
+            this.srpSearch.OnSearchClick += new PublicManager.Modules.SearchClickDelegate(this.srpSearch_OnSearchClick);
+            this.srpSearch.OnResetClick += new PublicManager.Modules.ResetClickDelegate(this.srpSearch_OnResetClick);
+            this.srpSearch.OnExportToClick += new PublicManager.Modules.ExportToClickDelegate(this.srpSearch_OnExportToClick);
+            this.srpSearch.OnCustomButtonClick += new PublicManager.Modules.CustomButtonClickDelegate(this.srpSearch_OnCustomButtonClick);
+            // 
+            // gcGrid
+            // 
+            this.gcGrid.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gcGrid.Location = new System.Drawing.Point(3, 72);
+            this.gcGrid.MainView = this.dgvDetail;
+            this.gcGrid.Name = "gcGrid";
+            this.gcGrid.Size = new System.Drawing.Size(1414, 604);
+            this.gcGrid.TabIndex = 4;
+            this.gcGrid.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.dgvDetail});
+            // 
+            // dgvDetail
+            // 
+            this.dgvDetail.Columns.AddRange(new DevExpress.XtraGrid.Columns.GridColumn[] {
+            this.gridColumn1,
+            this.gridColumn2,
+            this.gridColumn23,
+            this.gridColumn3,
+            this.gridColumn4,
+            this.gridColumn5,
+            this.gridColumn6,
+            this.gridColumn7,
+            this.gridColumn8,
+            this.gridColumn9,
+            this.gridColumn10,
+            this.gridColumn22,
+            this.gridColumn21,
+            this.gridColumn11,
+            this.gridColumn24,
+            this.gridColumn12,
+            this.gridColumn13,
+            this.gridColumn14,
+            this.gridColumn15,
+            this.gridColumn16,
+            this.gridColumn17,
+            this.gridColumn18,
+            this.gridColumn19,
+            this.gridColumn20});
+            this.dgvDetail.FocusRectStyle = DevExpress.XtraGrid.Views.Grid.DrawFocusRectStyle.RowFullFocus;
+            this.dgvDetail.GridControl = this.gcGrid;
+            this.dgvDetail.Name = "dgvDetail";
+            this.dgvDetail.OptionsSelection.EnableAppearanceFocusedCell = false;
+            this.dgvDetail.OptionsView.ShowFilterPanelMode = DevExpress.XtraGrid.Views.Base.ShowFilterPanelMode.ShowAlways;
+            this.dgvDetail.RowCellStyle += new DevExpress.XtraGrid.Views.Grid.RowCellStyleEventHandler(this.dgvDetail_RowCellStyle);
+            // 
+            // gridColumn1
+            // 
+            this.gridColumn1.Caption = "版本";
+            this.gridColumn1.FieldName = "row1";
+            this.gridColumn1.Name = "gridColumn1";
+            this.gridColumn1.OptionsColumn.ShowInCustomizationForm = false;
+            // 
+            // gridColumn2
+            // 
+            this.gridColumn2.Caption = "类型";
+            this.gridColumn2.FieldName = "row2";
+            this.gridColumn2.Name = "gridColumn2";
+            this.gridColumn2.OptionsColumn.ShowInCustomizationForm = false;
+            // 
+            // gridColumn23
+            // 
+            this.gridColumn23.Caption = "项目领域/技术方向";
+            this.gridColumn23.FieldName = "row23";
+            this.gridColumn23.Name = "gridColumn23";
+            this.gridColumn23.Visible = true;
+            this.gridColumn23.VisibleIndex = 0;
+            this.gridColumn23.Width = 274;
+            // 
+            // gridColumn3
+            // 
+            this.gridColumn3.Caption = "申报领域";
+            this.gridColumn3.FieldName = "row3";
+            this.gridColumn3.Name = "gridColumn3";
+            this.gridColumn3.Width = 59;
+            // 
+            // gridColumn4
+            // 
+            this.gridColumn4.Caption = "计划批次";
+            this.gridColumn4.FieldName = "row4";
+            this.gridColumn4.Name = "gridColumn4";
+            this.gridColumn4.Visible = true;
+            this.gridColumn4.VisibleIndex = 1;
+            this.gridColumn4.Width = 165;
+            // 
+            // gridColumn5
+            // 
+            this.gridColumn5.Caption = "项目编号";
+            this.gridColumn5.FieldName = "row5";
+            this.gridColumn5.Name = "gridColumn5";
+            this.gridColumn5.Visible = true;
+            this.gridColumn5.VisibleIndex = 2;
+            this.gridColumn5.Width = 156;
+            // 
+            // gridColumn6
+            // 
+            this.gridColumn6.Caption = "项目名称";
+            this.gridColumn6.FieldName = "row6";
+            this.gridColumn6.Name = "gridColumn6";
+            this.gridColumn6.Visible = true;
+            this.gridColumn6.VisibleIndex = 3;
+            this.gridColumn6.Width = 253;
+            // 
+            // gridColumn7
+            // 
+            this.gridColumn7.Caption = "总经费";
+            this.gridColumn7.FieldName = "row7";
+            this.gridColumn7.Name = "gridColumn7";
+            this.gridColumn7.Width = 56;
+            // 
+            // gridColumn8
+            // 
+            this.gridColumn8.Caption = "周期";
+            this.gridColumn8.FieldName = "row8";
+            this.gridColumn8.Name = "gridColumn8";
+            this.gridColumn8.Width = 56;
+            // 
+            // gridColumn9
+            // 
+            this.gridColumn9.Caption = "密级";
+            this.gridColumn9.FieldName = "row9";
+            this.gridColumn9.Name = "gridColumn9";
+            this.gridColumn9.Width = 56;
+            // 
+            // gridColumn10
+            // 
+            this.gridColumn10.Caption = "项目负责人";
+            this.gridColumn10.FieldName = "row10";
+            this.gridColumn10.Name = "gridColumn10";
+            // 
+            // gridColumn22
+            // 
+            this.gridColumn22.Caption = "项目负责人座机";
+            this.gridColumn22.FieldName = "row21";
+            this.gridColumn22.Name = "gridColumn22";
+            this.gridColumn22.Width = 92;
+            // 
+            // gridColumn21
+            // 
+            this.gridColumn21.Caption = "项目负责人手机";
+            this.gridColumn21.FieldName = "row22";
+            this.gridColumn21.Name = "gridColumn21";
+            this.gridColumn21.Width = 92;
+            // 
+            // gridColumn11
+            // 
+            this.gridColumn11.Caption = "项目牵头单位";
+            this.gridColumn11.FieldName = "row11";
+            this.gridColumn11.Name = "gridColumn11";
+            // 
+            // gridColumn24
+            // 
+            this.gridColumn24.Caption = "归一化单位";
+            this.gridColumn24.FieldName = "row24";
+            this.gridColumn24.Name = "gridColumn24";
+            // 
+            // gridColumn12
+            // 
+            this.gridColumn12.Caption = "牵头单位所属大单位";
+            this.gridColumn12.FieldName = "row12";
+            this.gridColumn12.Name = "gridColumn12";
+            this.gridColumn12.Width = 74;
+            // 
+            // gridColumn13
+            // 
+            this.gridColumn13.Caption = "牵头单位属地";
+            this.gridColumn13.FieldName = "row13";
+            this.gridColumn13.Name = "gridColumn13";
+            this.gridColumn13.Width = 59;
+            // 
+            // gridColumn14
+            // 
+            this.gridColumn14.Caption = "立项选题评议";
+            this.gridColumn14.FieldName = "row14";
+            this.gridColumn14.Name = "gridColumn14";
+            this.gridColumn14.Visible = true;
+            this.gridColumn14.VisibleIndex = 4;
+            this.gridColumn14.Width = 99;
+            // 
+            // gridColumn15
+            // 
+            this.gridColumn15.Caption = "立项审核";
+            this.gridColumn15.FieldName = "row15";
+            this.gridColumn15.Name = "gridColumn15";
+            this.gridColumn15.Visible = true;
+            this.gridColumn15.VisibleIndex = 5;
+            this.gridColumn15.Width = 74;
+            // 
+            // gridColumn16
+            // 
+            this.gridColumn16.Caption = "立项复议";
+            this.gridColumn16.FieldName = "row16";
+            this.gridColumn16.Name = "gridColumn16";
+            this.gridColumn16.Visible = true;
+            this.gridColumn16.VisibleIndex = 6;
+            this.gridColumn16.Width = 71;
+            // 
+            // gridColumn17
+            // 
+            this.gridColumn17.Caption = "合同审查等级";
+            this.gridColumn17.FieldName = "row17";
+            this.gridColumn17.Name = "gridColumn17";
+            this.gridColumn17.Visible = true;
+            this.gridColumn17.VisibleIndex = 7;
+            this.gridColumn17.Width = 99;
+            // 
+            // gridColumn18
+            // 
+            this.gridColumn18.Caption = "合同审核等级";
+            this.gridColumn18.FieldName = "row18";
+            this.gridColumn18.Name = "gridColumn18";
+            this.gridColumn18.Visible = true;
+            this.gridColumn18.VisibleIndex = 8;
+            this.gridColumn18.Width = 100;
+            // 
+            // gridColumn19
+            // 
+            this.gridColumn19.Caption = "备注";
+            this.gridColumn19.FieldName = "row19";
+            this.gridColumn19.Name = "gridColumn19";
+            this.gridColumn19.Visible = true;
+            this.gridColumn19.VisibleIndex = 9;
+            this.gridColumn19.Width = 105;
+            // 
+            // gridColumn20
+            // 
+            this.gridColumn20.Caption = "ProjectID";
+            this.gridColumn20.FieldName = "row20";
+            this.gridColumn20.Name = "gridColumn20";
+            this.gridColumn20.OptionsColumn.ShowInCustomizationForm = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.gcGrid);
+            this.groupBox1.Controls.Add(this.panel1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1420, 679);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            // 
+            // ModuleController
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.groupBox1);
+            this.Name = "ModuleController";
+            this.Size = new System.Drawing.Size(1420, 679);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.gcGrid)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDetail)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn2;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn1;
+        private DevExpress.XtraGrid.Views.Grid.GridView dgvDetail;
+        private DevExpress.XtraGrid.GridControl gcGrid;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn3;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn4;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn5;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn6;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn7;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn8;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn9;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn10;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn11;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn12;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn13;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn14;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn15;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn16;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn17;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn18;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn19;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn20;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn23;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn22;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn21;
+        private SearchRulePanel srpSearch;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn24;
+    }
+}
