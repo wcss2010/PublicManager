6d6d44b3b58f34cf6359119909b4d0c9f133914f
wcss <wcss2020@gmail.com>
2020-05-09 18:13:22 +0800

    ...

--- Code/PublicManager/PublicManager/Modules/DataLoad/ManagerInfoEdit/Froms/ModifyManagerInfoWithSelectedForm.Designer.cs
+++ Code/PublicManager/PublicManager/Modules/DataLoad/ManagerInfoEdit/Froms/ModifyManagerInfoWithSelectedForm.Designer.cs
@@ -40,7 +40,18 @@
             this.barButtonItem5 = new DevExpress.XtraBars.BarButtonItem();
             this.barButtonItem6 = new DevExpress.XtraBars.BarButtonItem();
             this.bsiBottomText = new DevExpress.XtraBars.BarStaticItem();
+            this.splitContainerControl1 = new DevExpress.XtraEditors.SplitContainerControl();
+            this.tlTestA = new System.Windows.Forms.TreeView();
+            this.panelControl1 = new DevExpress.XtraEditors.PanelControl();
+            this.btnCancel = new DevExpress.XtraEditors.SimpleButton();
+            this.btnOK = new DevExpress.XtraEditors.SimpleButton();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.txtContent = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.rcTopBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainerControl1)).BeginInit();
+            this.splitContainerControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).BeginInit();
+            this.panelControl1.SuspendLayout();
             this.SuspendLayout();
             // 
             // rcTopBar
@@ -74,7 +85,7 @@
             this.rcTopBar.ShowPageHeadersMode = DevExpress.XtraBars.Ribbon.ShowPageHeadersMode.Hide;
             this.rcTopBar.ShowQatLocationSelector = false;
             this.rcTopBar.ShowToolbarCustomizeItem = false;
-            this.rcTopBar.Size = new System.Drawing.Size(506, 25);
+            this.rcTopBar.Size = new System.Drawing.Size(923, 27);
             this.rcTopBar.Toolbar.ShowCustomizeItem = false;
             // 
             // barCheckItem1
@@ -145,20 +156,98 @@
             this.bsiBottomText.Name = "bsiBottomText";
             this.bsiBottomText.TextAlignment = System.Drawing.StringAlignment.Center;
             // 
-            // SelectedListCheckForm
+            // splitContainerControl1
+            // 
+            this.splitContainerControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainerControl1.Location = new System.Drawing.Point(0, 27);
+            this.splitContainerControl1.Name = "splitContainerControl1";
+            this.splitContainerControl1.Panel1.Controls.Add(this.tlTestA);
+            this.splitContainerControl1.Panel1.Text = "Panel1";
+            this.splitContainerControl1.Panel2.Controls.Add(this.txtContent);
+            this.splitContainerControl1.Panel2.Controls.Add(this.lblTitle);
+            this.splitContainerControl1.Panel2.Text = "Panel2";
+            this.splitContainerControl1.Size = new System.Drawing.Size(923, 313);
+            this.splitContainerControl1.SplitterPosition = 387;
+            this.splitContainerControl1.TabIndex = 1;
+            this.splitContainerControl1.Text = "splitContainerControl1";
+            // 
+            // tlTestA
+            // 
+            this.tlTestA.CheckBoxes = true;
+            this.tlTestA.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tlTestA.Location = new System.Drawing.Point(0, 0);
+            this.tlTestA.Name = "tlTestA";
+            this.tlTestA.Size = new System.Drawing.Size(387, 313);
+            this.tlTestA.TabIndex = 1;
+            // 
+            // panelControl1
+            // 
+            this.panelControl1.Controls.Add(this.btnCancel);
+            this.panelControl1.Controls.Add(this.btnOK);
+            this.panelControl1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panelControl1.Location = new System.Drawing.Point(0, 340);
+            this.panelControl1.Name = "panelControl1";
+            this.panelControl1.Size = new System.Drawing.Size(923, 56);
+            this.panelControl1.TabIndex = 2;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(555, 8);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(107, 42);
+            this.btnCancel.TabIndex = 9;
+            this.btnCancel.Text = "取消";
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(282, 8);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(107, 42);
+            this.btnOK.TabIndex = 8;
+            this.btnOK.Text = "确定";
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblTitle.Font = new System.Drawing.Font("宋体", 12F);
+            this.lblTitle.Location = new System.Drawing.Point(0, 0);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(531, 36);
+            this.lblTitle.TabIndex = 10;
+            this.lblTitle.Text = "label1";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // txtContent
+            // 
+            this.txtContent.Dock = System.Windows.Forms.DockStyle.Top;
+            this.txtContent.Location = new System.Drawing.Point(0, 36);
+            this.txtContent.Multiline = true;
+            this.txtContent.Name = "txtContent";
+            this.txtContent.Size = new System.Drawing.Size(531, 74);
+            this.txtContent.TabIndex = 11;
+            // 
+            // ModifyManagerInfoWithSelectedForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(506, 449);
+            this.ClientSize = new System.Drawing.Size(923, 396);
+            this.Controls.Add(this.splitContainerControl1);
+            this.Controls.Add(this.panelControl1);
             this.Controls.Add(this.rcTopBar);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
-            this.Name = "SelectedListCheckForm";
+            this.Name = "ModifyManagerInfoWithSelectedForm";
             this.Ribbon = this.rcTopBar;
             this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "批量修改";
+            this.Text = "批量修改项目领域/技术方向或计划批次";
             ((System.ComponentModel.ISupportInitialize)(this.rcTopBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainerControl1)).EndInit();
+            this.splitContainerControl1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).EndInit();
+            this.panelControl1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -178,5 +267,12 @@
         private DevExpress.XtraBars.BarButtonItem barButtonItem5;
         private DevExpress.XtraBars.BarButtonItem barButtonItem6;
         private DevExpress.XtraBars.BarStaticItem bsiBottomText;
+        private DevExpress.XtraEditors.SplitContainerControl splitContainerControl1;
+        private System.Windows.Forms.TreeView tlTestA;
+        private DevExpress.XtraEditors.PanelControl panelControl1;
+        private DevExpress.XtraEditors.SimpleButton btnCancel;
+        private DevExpress.XtraEditors.SimpleButton btnOK;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.TextBox txtContent;
     }
 }
--- Code/PublicManager/PublicManager/Modules/DataLoad/ManagerInfoEdit/Froms/ModifyManagerInfoWithSelectedForm.cs
+++ Code/PublicManager/PublicManager/Modules/DataLoad/ManagerInfoEdit/Froms/ModifyManagerInfoWithSelectedForm.cs
@@ -1,4 +1,6 @@
 ﻿using DevExpress.XtraBars.Ribbon;
+using PublicManager.DB;
+using PublicManager.DB.Entitys;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -13,17 +15,58 @@ namespace PublicManager.Modules.DataLoad.ManagerInfoEdit.Froms
     public partial class ModifyManagerInfoWithSelectedForm : RibbonForm
     {
         private SearchRulePanel srpSearch;
-        public ModifyManagerInfoWithSelectedForm(SearchRulePanel srpPanel)
+        public ModifyManagerInfoWithSelectedForm(SearchRulePanel srpPanel,string labelText)
         {
             InitializeComponent();
 
+            this.lblTitle.Text = labelText;
             this.srpSearch = srpPanel;
             loadDate();
         }
 
         private void loadDate()
         {
-            
+            #region 加载项目树
+            tlTestA.Nodes.Clear();
+            List<Catalog> catalogList = ConnectionManager.Context.table("Catalog").where("(1=1)" + srpSearch.CatalogIDFilterString).select("*").getList<Catalog>(new Catalog());
+            foreach (Catalog catalog in catalogList)
+            {
+                Project proj = ConnectionManager.Context.table("Project").where("CatalogID='" + catalog.CatalogID + "'").select("*").getItem<Project>(new Project());
+
+                TreeNode parentNode = new TreeNode();
+                parentNode.Text = proj.ProjectName;
+                parentNode.Tag = proj;
+                tlTestA.Nodes.Add(parentNode);
+            }
+            #endregion
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            foreach (TreeNode tn in tlTestA.Nodes)
+            {
+                if (tn.Checked)
+                {
+                    Project proj = (Project)tn.Tag;
+                    switch (lblTitle.Text.Trim())
+                    {
+                        case "项目领域/技术方向:":
+                            proj.ProjectRange = txtContent.Text;
+                            break;
+                        case "计划批次:":
+                            proj.TaskNumber = txtContent.Text;
+                            break;
+                    }
+                    proj.copyTo(ConnectionManager.Context.table("Project")).where("ProjectID='" + proj.ProjectID + "'").update();
+                }
+            }
+
+            DialogResult = System.Windows.Forms.DialogResult.OK;
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            DialogResult = System.Windows.Forms.DialogResult.Cancel;
         }
     }
 }
